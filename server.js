48721fb62e7d32e406eb0a7bb3bd74628d280d6e
stefaleon <stefaleon@gmail.com>
2022-06-28 23:34:48 +0300

    Logout and not-found routes

--- server.js
+++ server.js
@@ -49,6 +49,13 @@ myDB(async (client) => {
   app.route('/profile').get(ensureAuthenticated, (req, res) => {
     res.render(process.cwd() + '/views/pug/profile', { username: req.user.username });
   });
+  app.route('/logout').get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+  app.use((req, res, next) => {
+    res.status(404).type('text').send('Not Found');
+  });
   passport.serializeUser((user, done) => {
     done(null, user._id);
   });
