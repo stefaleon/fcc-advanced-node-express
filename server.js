cf3c47144967f5c4661588dbd5a35fc9aac41138
stefaleon <stefaleon@gmail.com>
2022-06-28 21:21:08 +0300

    Authentication Strategies

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const ObjectID = require('mongodb').ObjectID;
+const LocalStrategy = require('passport-local');
 
 const app = express();
 
@@ -42,6 +43,17 @@ myDB(async (client) => {
       done(null, doc);
     });
   });
+  passport.use(new LocalStrategy(
+    function(username, password, done) {
+      myDataBase.findOne({ username: username }, function (err, user) {
+        console.log('User '+ username +' attempted to log in.');
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        if (password !== user.password) { return done(null, false); }
+        return done(null, user);
+      });
+    }
+  ));
 }).catch((e) => {
   app.route('/').get((req, res) => {
     res.render('pug', { title: e, message: 'Unable to login' });
