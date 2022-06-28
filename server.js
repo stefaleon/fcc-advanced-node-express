5c89a5a0588a23af17292cd7bd96625c8758298f
stefaleon <stefaleon@gmail.com>
2022-06-28 19:47:28 +0300

    Serialization of a User Object

--- server.js
+++ server.js
@@ -24,6 +24,16 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    done(null, null);
+  // });
+});
+
 app.route('/').get((req, res) => {
   // res.render(process.cwd() + '/views/pug');
   res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
