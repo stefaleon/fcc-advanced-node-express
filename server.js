fbfb765485aa3e5b4bb8425e9bc74ffdc63035cc
stefaleon <stefaleon@gmail.com>
2022-06-28 23:43:59 +0300

    Register

--- server.js
+++ server.js
@@ -40,9 +40,32 @@ myDB(async (client) => {
     res.render('pug', {
       title: 'Connected to Database',
       message: 'Please login',
-      showLogin: true
+      showLogin: true,
+      showRegistration: true
     });
   });
+  app.route('/register').post(
+    (req, res, next) => {
+      myDataBase.findOne({ username: req.body.username }, function (err, user) {
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect('/');
+        } else {
+          myDataBase.insertOne({ username: req.body.username, password: req.body.password }, (err, doc) => {
+            if (err) {
+              res.redirect('/');
+            } else {
+              next(null, doc.ops[0]);
+            }
+          });
+        }
+      });
+    },passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile');
+    }
+  );
   app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
     res.redirect('/profile');
   });
