e62759a9b47e901a1863af1d7c391c0f510901f5
stefaleon <stefaleon@gmail.com>
2022-06-28 23:31:41 +0300

    Pass username to profile pug

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ myDB(async (client) => {
     res.redirect('/profile');
   });
   app.route('/profile').get(ensureAuthenticated, (req, res) => {
-    res.render(process.cwd() + '/views/pug/profile');
+    res.render(process.cwd() + '/views/pug/profile', { username: req.user.username });
   });
   passport.serializeUser((user, done) => {
     done(null, user._id);
