ded9f681be9205e89acd1d3aa334804b2f29af54
stefaleon <stefaleon@gmail.com>
2022-06-29 15:54:55 +0300

    Setup sockets, create route to chat pug and redirect to it on Github login

--- routes.js
+++ routes.js
@@ -43,13 +43,17 @@ module.exports = function (app, myDataBase) {
       );
       app.route('/auth/github').get(passport.authenticate('github'));
       app.route('/auth/github/callback').get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
-        res.redirect('/profile');
-      });    
+        req.session.user_id = req.user.id;
+        res.redirect('/chat');
+      }); 
       app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
         res.redirect('/profile');
       });
       app.route('/profile').get(ensureAuthenticated, (req, res) => {
         res.render(process.cwd() + '/views/pug/profile', { username: req.user.username });
+      });
+      app.route('/chat').get(ensureAuthenticated, (req, res) => {
+        res.render('pug/chat', { user: req.user });
       });
       app.route('/logout').get((req, res) => {
         req.logout();
--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const routes = require('./routes.js');
 const auth = require('./auth.js');
 
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.set('view engine', 'pug');
 
@@ -34,7 +36,9 @@ myDB(async (client) => {
   routes(app, myDataBase);
   auth(app, myDataBase);
   
- 
+  io.on('connection', (socket) => {
+    console.log('A user has connected');
+  }); 
 }).catch((e) => {
   app.route('/').get((req, res) => {
     res.render('pug', { title: e, message: 'Unable to login' });
@@ -42,6 +46,6 @@ myDB(async (client) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
